1ac071ce5745ff98b51ba861113a99d586f626ac
Bhanu pratap singh <pbhanu314@gmail.com>
2021-05-02 11:24:39 +0530

    Button added

--- src/Pages/Create.js
+++ src/Pages/Create.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
 
 export default function Create() {
     return (
-        <div>
+        <Container>
             <Typography
                 variant='h6'
                 component='h2'
@@ -12,6 +14,20 @@ export default function Create() {
             >
                 Create a New Note
             </Typography>
-        </div>
+            <Button
+                onClick={() => console.log("Clicked")}
+                type='submit'
+                color='secondary'
+                variant='contained'
+            >
+                Submit
+            </Button>
+            {/* <Button type='submit' variant='outlined' color='secondary' > Submit</Button>
+            <ButtonGroup color="primary" variant="contained" aria-label="contained primary button group">
+                <Button>One</Button>
+                <Button>Two</Button>
+                <Button>Three</Button>
+            </ButtonGroup> */}
+        </Container>
     )
 }
