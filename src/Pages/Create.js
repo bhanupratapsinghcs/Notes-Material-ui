64d097a8e4d76f2ddcc026ad7dd22d50a6e3faea
Bhanu pratap singh <pbhanu314@gmail.com>
2021-05-02 11:44:49 +0530

    useStyle and icon

--- src/Pages/Create.js
+++ src/Pages/Create.js
@@ -2,11 +2,19 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+
+})
 
 export default function Create() {
+    const classes = useStyles()
     return (
         <Container>
             <Typography
+                className={classes.title}
                 variant='h6'
                 component='h2'
                 gutterBottom
@@ -15,10 +23,13 @@ export default function Create() {
                 Create a New Note
             </Typography>
             <Button
+                className={classes.btn}
                 onClick={() => console.log("Clicked")}
                 type='submit'
-                color='secondary'
+                color='primary'
                 variant='contained'
+                // startIcon={<SendIcon />}
+                endIcon={<KeyboardArrowRightIcon />}
             >
                 Submit
             </Button>
