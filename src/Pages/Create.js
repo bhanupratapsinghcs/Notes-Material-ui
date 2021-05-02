adc7c823259202c77287db9b471200492a490522
Bhanu pratap singh <pbhanu314@gmail.com>
2021-05-02 11:13:02 +0530

    Typography Component added to create page

--- src/Pages/Create.js
+++ src/Pages/Create.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
 
 export default function Create() {
     return (
         <div>
-            Create Page
+            <Typography
+                variant='h6'
+                component='h2'
+                gutterBottom
+                color='textSecondary'
+            >
+                Create a New Note
+            </Typography>
         </div>
     )
 }
