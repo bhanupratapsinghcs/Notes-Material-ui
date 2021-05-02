a395a4069f605784dd6bdc36d97d3933a0a8cce5
Bhanu pratap singh <pbhanu314@gmail.com>
2021-05-02 18:19:39 +0530

    masonry added

--- src/Pages/Notes.js
+++ src/Pages/Notes.js
@@ -1,7 +1,9 @@
-import { Container, Grid, Paper } from '@material-ui/core'
+import { Container, Grid } from '@material-ui/core'
 import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
+import NoteCard from '../Components/NoteCard'
+import Masonry from "react-masonry-css"
 
 export default function Notes() {
 
@@ -12,19 +14,35 @@ export default function Notes() {
             .then(res => res.json())
             .then(data => setNotes(data))
     }, [])
+
+    const handleDelete = async (id) => {
+        await fetch('http://localhost:8000/notes/' + id, {
+            method: 'DELETE'
+        })
+        const newNotes = notes.filter(note => note.id != id)
+        setNotes(newNotes)
+    }
+    const breakpoint = {
+        default: 3,
+        1100: 2,
+        720: 1
+    }
     return (
         <Container>
-            <Grid container>
+            <Masonry
+                breakpointCols={3}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column">
+                {/* array of JSX items */}
+
                 {
                     notes.map(note => (
-                        <Grid item key={note.id} xs={12} md={6} lg={4}>
-                            <Paper>
-                                {note.title}
-                            </Paper>
-                        </Grid>
+                        <div item key={note.id} xs={12} md={6} lg={4}>
+                            <NoteCard note={note} handleDelete={handleDelete} />
+                        </div>
                     ))
                 }
-            </Grid>
+            </Masonry>
         </Container>
     )
 }
